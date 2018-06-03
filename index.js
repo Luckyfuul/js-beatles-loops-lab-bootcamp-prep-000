c22bc9a85502d714f74b6ead13a5fd45617f9447
TJ Zuvic <terrance@zuvmarketing.com>
2018-06-03 09:47:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ function johnLennonFacts(facts){
 }
 function iLoveTheBeatles(n){
   var newArray = []
-  do { newArray.push("I love the Beatles!")
+  do { newArray.push("I love the Beatles!");
+  ++n;
     }
   while(n < 15){
     ++n;
